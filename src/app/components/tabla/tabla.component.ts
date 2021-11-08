3a84356f4e09fc9f9b2e16fdb23ddb17046a1733
JoaquinRBP <sacrificiodecorreo@gmail.com>
2021-11-08 12:51:26 -0500

    Coponente tabla

--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {COLUMN_DATA, ELEMENT_DATA} from '../../utils';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-tabla',
   templateUrl: './tabla.component.html',
   styleUrls: ['./tabla.component.scss']
 })
 export class TablaComponent implements OnInit {
-
+  @Input() val:string[]=[];
+  @Input() datos:any;
+  displayedColumns: string[] = [];
+  dataSource:any;
+  nuevo:any;
+  acciones=['Ver','Editar','Eliminar'];
   constructor() { }
 
   ngOnInit(): void {
+    if(this.val.length>0){
+      this.displayedColumns=this.val.concat(this.acciones);
+      this.dataSource=this.datos;
+    }else{
+      this.displayedColumns=COLUMN_DATA.concat(this.acciones);
+      this.dataSource=ELEMENT_DATA;
+    }
+  }
+  ver(element:any){
+    console.log('Viendo', element['Id'])
+  }
+  editar(element:any){
+    console.log('Editando', element['Id'])
+  }
+  eliminar(element:any){
+    //Se está recibiendo todo el array de columna, asi que no se olvide los campos
+    //Tener en cuenta que se retorna el elemento completo del datasource
+    //Ejemplo = console.log(element['Contenido1'])
+    console.log('Eliminando', element['Id']);
+
   }
 
 }
---
+++ src/app/utils.ts
@@ -0,0 +1,9 @@
+export const  ELEMENT_DATA= [
+  {Id:0, Titulo1: '' , Contenido1: ''},
+  {Id:1, Titulo1: '' , Contenido1: ''},
+  {Id:2, Titulo1: '' , Contenido1: ''},
+  {Id:3, Titulo1: '' , Contenido1: ''},
+  {Id:4, Titulo1: '' , Contenido1: ''},
+];
+
+export const COLUMN_DATA=['Id','Titulo1', 'Contenido1'];
