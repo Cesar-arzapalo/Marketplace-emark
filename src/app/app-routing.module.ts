9c5d625012637cb3bdbd021030fe99b387b5be9d
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2021-11-08 20:56:55 -0500

    sección de login y registro de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   {path:"catalogo",  loadChildren: () => import('./pages/catalogo/catalogo.module').then( m => m.CatalogoModule)},
   {path:"administracion",  loadChildren: () => import('./pages/administracion/administracion.module').then( m => m.AdministracionModule)},
   {path:"proveedor",  loadChildren: () => import('./pages/proveedor/proveedor.module').then( m => m.ProveedorModule)},
+  {path:"auth",  loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)},
   {path: "**", pathMatch:"full", redirectTo:"/catalogo"}
 ];
 
---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {
+		path: '',
+		component: AuthComponent,
+		children: [
+			{
+				path: 'register',
+				component: RegisterComponent,
+			},
+			{
+				path: 'login',
+				component: LoginComponent,
+			},
+			{
+				path: "**", 
+				pathMatch:"full", 
+				redirectTo:"login"
+			}
+		]
+	}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
