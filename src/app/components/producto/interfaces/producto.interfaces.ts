ddd5842597dce7358f5b615c9f877cda39672780
Hiro-Cabello <ronaljairocabellocordova@gmail.com>
2021-11-09 22:08:13 -0500

    intentoservice

---
+++ src/app/components/producto/interfaces/producto.interfaces.ts
@@ -0,0 +1,13 @@
+export interface Producto{
+    nombre:string;
+    descripcion:string;
+    caracteristicas:string;
+    unidad:string;
+    precioUnidad:number;
+    stock:number,
+    valoracion:number,
+    visitas:number,
+    //idCategoria:string
+    //idProveedor:string
+    imagenes:string[]
+}
