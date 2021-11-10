d6a6dd1fbc369b2e210fb3f911db85e15cbeb500
JoaquinRBP <sacrificiodecorreo@gmail.com>
2021-11-09 22:45:52 -0500

    Modificaciones tabla

--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {COLUMN_DATA, ELEMENT_DATA} from '../../utils';
+import {COLUMN_DATA, ELEMENT_DATA, Producto} from '../../utils';
 import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-tabla',
@@ -10,19 +10,20 @@ export class TablaComponent implements OnInit {
   @Input() val:string[]=[];
   @Input() datos:any;
   displayedColumns: string[] = [];
-  dataSource:any;
-  nuevo:any;
+  data:any=[];
+  dataSource = new MatTableDataSource<Producto>(this.data);
   acciones=['Ver','Editar','Eliminar'];
   constructor() { }
 
   ngOnInit(): void {
     if(this.val.length>0){
       this.displayedColumns=this.val.concat(this.acciones);
-      this.dataSource=this.datos;
+      this.data=this.datos;
     }else{
       this.displayedColumns=COLUMN_DATA.concat(this.acciones);
-      this.dataSource=ELEMENT_DATA;
+      this.data=ELEMENT_DATA;
     }
+    this.dataSource = new MatTableDataSource<Producto>(this.data);
   }
   ver(element:any){
     console.log('Viendo', element['Id'])
@@ -34,6 +35,8 @@ export class TablaComponent implements OnInit {
     //Se está recibiendo todo el array de columna, asi que no se olvide los campos
     //Tener en cuenta que se retorna el elemento completo del datasource
     //Ejemplo = console.log(element['Contenido1'])
+    this.data.splice(element['Id'],1)
+    this.dataSource = new MatTableDataSource<Producto>(this.data);
     console.log('Eliminando', element['Id']);
 
   }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -5,5 +5,9 @@ export const  ELEMENT_DATA= [
   {Id:3, Titulo1: '' , Contenido1: ''},
   {Id:4, Titulo1: '' , Contenido1: ''},
 ];
-
+export interface Producto {
+  Id: number;
+  Titulo1: string;
+  Contenido1: string;
+}
 export const COLUMN_DATA=['Id','Titulo1', 'Contenido1'];
