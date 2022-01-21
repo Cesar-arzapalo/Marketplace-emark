065f2a41d16bf01aaa83fbadbcea0fcd29db160a
Aptir <cesar.arzapalo@unmsm.edu.pe>
2022-01-21 17:48:10 -0500

    gustos dulces titulo cambiado

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/aplication-emark'));
+app.use(express.static(__dirname+'/dist/Gustos-Dulces'));
 app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/aplication-emark/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/Gustos-Dulces/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
