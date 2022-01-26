adf6f32f64e6353d276ccad5dbf0440ab2806cb8
Aptir <cesar.arzapalo@unmsm.edu.pe>
2022-01-26 17:13:14 -0500

    eliminando librerias adicionales

--- src/app/pages/catalogo/catalogo.module.ts
+++ src/app/pages/catalogo/catalogo.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { CatalogoRoutingModule } from './catalogo-routing.module';
 import { CatalogoComponent } from './catalogo.component';
 import { ComponentsModule } from '../../components/components.module';
-import { MaterialModule } from 'src/app/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
@@ -14,7 +13,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     CatalogoRoutingModule,
     ComponentsModule,
-    MaterialModule,
     ReactiveFormsModule
   ]
 })
